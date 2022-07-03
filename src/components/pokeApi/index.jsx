f1d7931873df5907339c99cb84f66fe941375754
André Lusegardis <lusegardis@gmail.com>
2022-07-03 00:11:38 -0300

    added '0's to match the number of digits of apiData.length in the number with a smaller length

--- src/components/pokeApi/index.jsx
+++ src/components/pokeApi/index.jsx
@@ -20,6 +20,10 @@ export const PokeApi = () => {
       <ul>
         {apiData.map((e, index) => (
           <li key={index}>
+            {index.toString().length < apiData.length.toString().length &&
+              "0".repeat(
+                apiData.length.toString().length - index.toString().length
+              )}
             {index}: {e.name.charAt(0).toUpperCase() + e.name.slice(1)}
           </li>
         ))}
